a834673b7f05fbe1de1db4abf0068bdc03005170
K.S.KAUSHIKRAM <139365426+KSKAUSHIKRAM@users.noreply.github.com>
2025-04-05 22:20:04 +0530

    front page
    development

--- churn_predict/src/App.js
+++ churn_predict/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// Import your new images
+import NewImage1 from "./Machine1.png";
+import NewImage2 from "./Machine2.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {/* Background Images */}
+        <img src={NewImage1} alt="Graphic 1" className="image top-left" />
+        <img src={NewImage2} alt="Graphic 2" className="image bottom-right" />
+
+        {/* Content Box */}
+        <div className="box">
+          <p className="bold-text">Hello All !!!</p>
+          <p>This is a churn Prediction ML</p>
+          <button className="blue-button">Let's Start</button>
+        </div>
+      </div>
     </div>
   );
 }
