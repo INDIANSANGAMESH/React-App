c23e326a304ace164999daf440c95c4e32776e56
INDIANSANGAMESH <sangamesh27115@gmail.com>
2025-04-05 22:30:32 +0530

    sangamesh
    2nd page modified

--- churn_predict/src/App.js
+++ churn_predict/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
 
 // Import your new images
 import NewImage1 from "./Machine1.png";
 import NewImage2 from "./Machine2.png";
 
-function App() {
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleStartClick = () => {
+    navigate("/next"); // Redirects to the next page
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -17,10 +24,31 @@ function App() {
         <div className="box">
           <p className="bold-text">Hello All !!!</p>
           <p>This is a churn Prediction ML</p>
-          <button className="blue-button">Let's Start</button>
+          <button className="blue-button" onClick={handleStartClick}>
+            Let's Start
+          </button>
         </div>
       </div>
     </div>
+  );
+};
+
+const NextPage = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to the Next Page!</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/next" element={<NextPage />} />
+      </Routes>
+    </Router>
   );
 }
 
